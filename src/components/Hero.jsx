9b0be0840ea40bec7bfca0bad8516678f7ae71ab
Besart Ramadani <besartr1995@gmail.com>
2026-04-28 22:32:15 +0200

    small fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,8 +58,6 @@ export default function Hero() {
 
           <div className="hero-stats">
             <div className="hero-stat">
-            </div>
-            <div className="hero-stat">
               <span className="hero-stat-num">2+</span>
               <span className="hero-stat-lbl">Years Exp</span>
             </div>
